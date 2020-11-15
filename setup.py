3afea94c27e85cb50792a8d4e45ca22777fa7c3a
Cooper Lees <me@cooperlees.com>
2020-11-14 23:16:00 -0800

    Move to pyhton@v2 GitHub + Test in Python 3.9 - Use newer GitHub action for Python - Add 3.9 into test matrix

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     python_requires=">=3.6",
     install_requires=None,
